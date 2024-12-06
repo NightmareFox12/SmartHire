e6f214f6b466d3dd24de2b3a8e77395455e4005a
NightmareFox <carloshenriquez392004@gmail.com>
2024-12-06 13:55:19 -0400

    add deploy_task

--- packages/hardhat/deploy/00_deploy_task.ts
+++ packages/hardhat/deploy/00_deploy_task.ts
@@ -1,15 +1,43 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
+//import { Contract } from "ethers";
+
+const deployTask: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const allAccounts = await hre.getUnnamedAccounts();
+  const { deploy } = hre.deployments;
+
+  await deploy("TaskContract", {
+    from: allAccounts[0],
+    args: ["0xD2692F9df925D18D527ABe8b3d99EE9E9C8d75AE"],
+    log: true,
+    // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
+    // automatically mining the contract deployment transaction. There is no effect on live networks.
+    autoMine: true,
+  });
+
+  // Get the deployed contract to interact with it after deploying.
+  // const taskContract = await hre.ethers.getContract<Contract>(
+  //   "TaskContract",
+  //   "0x545eC13A0D736474BCF62693322168f161a00447",
+  // );
+  // console.log("👋 Initial greeting:", await taskContract.greeting());
+  console.log("deploy with address", "0x545eC13A0D736474BCF62693322168f161a00447", "allAccounts.map(x => x)");
+};
+
+export default deployTask;
+deployTask.tags = ["TaskContract"];
+
+/*
+00_deploy_your_contract.ts
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
 import { Contract } from "ethers";
 
-/**
  * Deploys a contract named "YourContract" using the deployer account and
  * constructor arguments set to the deployer address
  *
  * @param hre HardhatRuntimeEnvironment object.
- */
 const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  /*
     On localhost, the deployer account is the one that comes with Hardhat, which is already funded.
 
     When deploying to live networks (e.g `yarn deploy --network sepolia`), the deployer account
@@ -18,7 +46,7 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
     You can generate a random account with `yarn generate` which will fill DEPLOYER_PRIVATE_KEY
     with a random private key in the .env file (then used on hardhat.config.ts)
     You can run the `yarn account` command to check your balance in every network.
-  */
+
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
@@ -42,3 +70,5 @@ export default deployYourContract;
 // Tags are useful if you have multiple deploy files and only want to run one of them.
 // e.g. yarn deploy --tags YourContract
 deployYourContract.tags = ["YourContract"];
+
+*/
