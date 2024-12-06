7b02640cc9e997d992df41653d71d9fae907e97d
Echizen512 <jmrm19722@gmail.com>
2024-12-06 15:55:11 -0400

    Change Page

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from "next";
 import ModalMetamask from "~~/components/ModalMetamask";
+import UserTaskList from "./_components/UserTaskList";
 
 const Home: NextPage = () => {
   return (
     <>
       <ModalMetamask />
+      <UserTaskList />
     </>
   );
 };
